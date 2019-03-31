30d7ce7f402628ea7374db51aaf36403876b85a5
Sergio Prada <ing.seprab@gmail.com>
2019-03-31 15:52:14 -0500

    Multilenguaje funcional!

--- SepraB - Innovation Development_files/index.js
+++ SepraB - Innovation Development_files/index.js
@@ -1,18 +1,28 @@
-$(document).ready(Init());
 
-function Init()
+function init()
 {
-  alert('started');
+  //alert('started');
+  espanol();
 }
 function ingles()
 {
-  alert('ingles');
-  //$(document).getElementById('ingles').style.visibility: visible;
-  //$(document).getElementById('espanol').style.visibility: hidden;
+  //alert('ingles');
+  document.getElementById('en_Content').style.display = 'initial';
+  document.getElementById('en_Firma').style.display = 'initial';
+  document.getElementById('en_Derechos').style.display = 'initial';
+
+  document.getElementById('es_Content').style.display = 'none';
+  document.getElementById('es_Firma').style.display = 'none';
+  document.getElementById('es_Derechos').style.display = 'none';
 }
 function espanol()
 {
-  alert('espanol');
-  //$(document).getElementById('ingles').style.visibility: hidden;
-  //$(document).getElementById('espanol').style.visibility: visible;
+  //alert('espanol');
+  document.getElementById('es_Firma').style.display = 'initial';
+  document.getElementById('es_Content').style.display = 'initial';
+  document.getElementById('es_Derechos').style.display = 'initial';
+
+  document.getElementById('en_Firma').style.display = 'none';
+  document.getElementById('en_Content').style.display = 'none';
+  document.getElementById('en_Derechos').style.display = 'none';
 }
