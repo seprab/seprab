a8ed3541b18602956c5fd45ca89490cb45ca6c78
ingseprab <sergioeprada@hotmail.com>
2019-03-31 14:45:08 -0500

    Desarrollo en proceso para multilenguaje

---
+++ SepraB - Innovation Development_files/index.js
@@ -0,0 +1,18 @@
+$(document).ready(Init());
+
+function Init()
+{
+  alert('started');
+}
+function ingles()
+{
+  alert('ingles');
+  //$(document).getElementById('ingles').style.visibility: visible;
+  //$(document).getElementById('espanol').style.visibility: hidden;
+}
+function espanol()
+{
+  alert('espanol');
+  //$(document).getElementById('ingles').style.visibility: hidden;
+  //$(document).getElementById('espanol').style.visibility: visible;
+}
