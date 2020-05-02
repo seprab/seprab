8abbe421110b5fd3c9ca66019cc660dcad2cea39
seprab <sergioeprada@hotmail.com>
2020-05-02 18:32:33 -0500

    Cambio de pag web a un template encontrado en https://github.com/zubairehman/Portfolio-Demo

---
+++ lib/constants/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Sepra';
+  static const String o = 'B';
+  static const String headline = 'I am Sergio Prada, Extended Reality development oriented Multimedia Engineer';
+    static const String summary = 'Result-oriented professional, providing satisfaction to the customers with innovation and development plans.  Proven experience in low and high development budget projects. Proficient at project planning, group and individual engineering problem solutions, code writing and implementation. Innovation and evolution of multimedia experiences.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved = '© 2019 SEPRAB. ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'About';
+  static const String menu_contact = 'Contact';
+
+}
